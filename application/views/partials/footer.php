13cdd365c870bdf47d9a3ac043f69eb05c0360d4
Domonskuy <obaybayusaputra@gmail.com>
2022-06-22 09:33:08 +0700

    Upload File

---
+++ application/views/partials/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>Copyright © <a href="https://instagram.com/obaybayusaputra" target="_blank">Bayu Saputra</a></span>
+		</div>
+	</div>
+</footer>
